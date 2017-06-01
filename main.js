89c8b3ff886877e381b771b2618fca78e2e04aec
dmcbrayer <dmcbrayer@gmail.com>
2017-06-01 13:37:53 -0400

    Wraps App component in a Provider component to connect to redux store.

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import App from './components/App'
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+const store = configureStore()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
-Expo.registerRootComponent(App);
+Expo.registerRootComponent(AppWithStore);
