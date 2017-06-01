91330f5b7eef0b1c20a57311b84b9e3594ee2f92
dmcbrayer <dmcbrayer@gmail.com>
2017-06-01 14:03:31 -0400

    Adds functionality to cache assets locally.

--- main.js
+++ main.js
@@ -3,13 +3,40 @@ import React from 'react';
 import { Provider } from 'react-redux'
 import configureStore from './store'
 import App from './components/App'
+import cacheAssets from './utils/cacheAssets'
 
 const store = configureStore()
 
-const AppWithStore = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+class AppWithStore extends React.Component {
+  // Move this to redux
+  state = {
+    appIsReady: false
+  }
+
+  componentWillMount() {
+    this._loadAssetsAsync()
+  }
+
+  _loadAssetsAsync = async () => {
+    try {
+      await cacheAssets({});
+    } catch(e) {
+      console.warn(
+        'There was an error caching assets (see: main.js), perhaps due to a ' +
+          'network timeout, so we skipped caching. Reload the app to try again.'
+      )
+    } finally {
+      this.setState({ appIsReady: true });
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  }
+}
 
 Expo.registerRootComponent(AppWithStore);
