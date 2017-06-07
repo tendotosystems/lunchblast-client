e4c4742512cf6d9d613bf811b385732555b4657f
dmcbrayer <dmcbrayer@gmail.com>
2017-06-06 22:39:47 -0400

    Adds a signup screen and navigation between signup and login.

--- navigators/AuthNavigator.js
+++ navigators/AuthNavigator.js
@@ -1,8 +1,10 @@
 import Login from '../screens/Login'
+import Signup from '../screens/Signup'
 import { StackNavigator } from 'react-navigation'
 
 const AuthNavigator = StackNavigator({
   Login: { screen: Login },
+  Signup: { screen: Signup }
 }, {
   headerMode: 'none'
 })
