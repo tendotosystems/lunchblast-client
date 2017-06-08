cbef41a873cd1326b8ab31dc43009ad9631c2eb9
dmcbrayer <dmcbrayer@gmail.com>
2017-06-08 16:50:43 -0400

    Addresses flex issues on Home screen.

--- components/Result.js
+++ components/Result.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
-    marginTop: 25,
+    marginVertical: 25,
     width: 250
   },
   destinationTitleStyle: {
